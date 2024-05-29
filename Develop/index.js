20f916eb1d4b6fcf2d9de13e4facaef5534c8b98
louis <louisdrippe@gmail.com>
2024-05-29 15:25:20 -0500

    Added Code

---
+++ Develop/index.js
@@ -0,0 +1,84 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: [
+            'MIT',
+            'Apache License 2.0',
+            'GNU General Public License v3.0',
+            'BSD 3-Clause "New" or "Revised" License',
+            'Mozilla Public License 2.0',
+            'GNU Lesser General Public License v3.0',
+            'GNU Affero General Public License v3.0',
+            'Eclipse Public License 2.0',
+            'ISC License',
+            'Creative Commons Zero v1.0 Universal',
+            'The Unlicense',
+            'None'
+        ],
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide contribution guidelines:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide test instructions:',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    },
+];
+
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('README.md generated successfully!')
+    );
+}
+
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        const markdown = generateMarkdown(data);
+        writeToFile('README.md', markdown);
+    });
+}
+
+// Initialize app
+init();
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,66 @@
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+      return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
+  }
+  return '';
+}
+
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+      return `\n* [License](#license)\n`;
+  }
+  return '';
+}
+
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+      return `## License
+
+This project is licensed under the ${license} license.`;
+  }
+  return '';
+}
+
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+${renderLicenseLink(data.license)}
+* [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+${renderLicenseSection(data.license)}
+
+## Questions
+
+For questions about the project, you can reach me at [${data.email}](mailto:${data.email}). Feel free to check out more of my work on [GitHub](https://github.com/${data.github}/).
+`;
+}
+
+module.exports = generateMarkdown;
