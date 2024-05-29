c82a46436986cdd15b02ec2fd41b21978bcadcea
louis <louisdrippe@gmail.com>
2024-05-29 16:03:39 -0500

    finishing touches to application

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,4 @@
+// Packages for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,5 @@
+// A function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== 'None') {
       return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
@@ -5,6 +7,8 @@ function renderLicenseBadge(license) {
   return '';
 }
 
+// A function that returns the license link
+// If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license !== 'None') {
       return `\n* [License](#license)\n`;
@@ -12,6 +16,8 @@ function renderLicenseLink(license) {
   return '';
 }
 
+// A function that returns the license section of README
+// If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license !== 'None') {
       return `## License
@@ -21,6 +27,7 @@ This project is licensed under the ${license} license.`;
   return '';
 }
 
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
